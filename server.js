9baf53cddf18fbe38904e6e930cba625a22d16c5
Onkar Chiroma <onkar.chiroma@MCL096.local>
2016-06-16 16:00:03 +0100

    deploy and basic redirect

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ var 	express		= require('express'),
     	app			= express(),
     	port        = process.env.PORT || 3000;
 
+
+//some basic redirect routes
+app.get('/', function(req, res) {
+  res.redirect('us/home/');
+});
+
+
 app.use(express.static(__dirname + '/build'));
 
 app.listen(port, function() {
