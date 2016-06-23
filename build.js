0a0efdad4a1c9d3752925895030734891a3d447a
pastorjuradotraverso <pastor.methods@gmail.com>
2016-06-23 19:30:25 +0100

    Add subfolder data to parent folder page

--- build.js
+++ build.js
@@ -35,83 +35,41 @@ var makeLangFolder = function (files, metalsmith, done) {
 
 var makeIndex = function (files, metalsmith, done) {
     var folders = [];
-    var filesWithLang = {};
-    var section = {};
-    var sector = {};
-    var subSector = {};
-    var cn = {};
-    section.cn = {name: '', image: '', thumbnail: '', heroVideo: '', fullVideo: '', heroImage: ''};
-    sector.cn = {name: '', image: '', thumbnail: '', heroVideo: '', fullVideo: '', heroImage: ''};
-    subSector.cn = {name: '', image: '', thumbnail: '', heroVideo: '', fullVideo: '', heroImage: ''};
-    var de = {};
-    section.de = {name: '', image: '', thumbnail: '', heroVideo: '', fullVideo: '', heroImage: ''};
-    sector.de = {name: '', image: '', thumbnail: '', heroVideo: '', fullVideo: '', heroImage: ''};
-    subSector.de = {name: '', image: '', thumbnail: '', heroVideo: '', fullVideo: '', heroImage: ''};
-    var us = {};
-    section.us = {name: '', image: '', thumbnail: '', heroVideo: '', fullVideo: '', heroImage: ''};
-    sector.us = {name: '', image: '', thumbnail: '', heroVideo: '', fullVideo: '', heroImage: ''};
-    subSector.us = {name: '', image: '', thumbnail: '', heroVideo: '', fullVideo: '', heroImage: ''};
+    var filesWithLang = [];
+    var levels = {};
     for (var file in files) {
         if (file.match(languages)) {
-            var folderPath = path.dirname(file);
-            folders.push(folderPath);
-            files[file].pathLang = folderPath;
-            var filePath = files[file].pathLang.split('/');
-            var lang = filePath[filePath.length - 1];
-            var filePathLength = files[file].pathLang.split('/').length;
-            if (filePathLength === 3) {
-                console.log(files[file])
-                var x = files[file].pathLang.split('/');
-                if (!section[lang].name) {
-                    section[lang].name = x[1];
-                }
-                if (!section[lang].image) {
-                    section[lang].image = files[file].image;
-                }
-                if (!section[lang].thumbnail) {
-                    section[lang].thumbnail = files[file].thumbnail;
-                }
-                if (!section[lang].heroVideo) {
-                    section[lang].heroVideo = files[file].heroVideo;
-                }
-                if (!section[lang].fullVideo) {
-                    section[lang].fullVideo = files[file].fullVideo;
-                }
-                if (section[lang].heroImage) {
-                    section[lang].heroImage = files[file].heroImage;
-                }
-            }
-            console.log(section.de)
-            if (filePathLength === 4) {
-                sector[lang].files.push(files[file]);
-            }
-            if (filePathLength === 5) {
-                subSector[lang].files.push(files[file]);
-            }
+            var folder = path.dirname(file);
+            files[file].pathLang = folder;
+            filesWithLang.push(files[file]);
+            folders.push(folder);
         }
     }
 
+    //find unique folder paths
+    folders = _.uniq(folders);
 
-    // concat the files in those paths
+    //concat the files in those paths
     for (var i = 0; i < folders.length; i++) {
         var folderPath = folders[i];
-        // var section = {};
-        // var sector = {};
-        // var subSector = {};
-        var folderUrlTmp = folderPath.split('/');
-        var lang = folderUrlTmp[folderUrlTmp.length - 1];
-        var filteredByLang = folders.filter(RegExp.prototype.test.bind(new RegExp(lang)));
-        // console.log(filteredByLang)
+        var folderName;
         var toIndex = '';
         var templateIndexName = '';
         var thumbnailPath = '';
         var heroVideoPath = '';
         var heroImagePath = '';
         var fullVideoPath = '';
+        var intro = '';
         var toToc = [];
         for (var j = 0; j < filesWithLang.length; j++) {
             var fileWithLang = filesWithLang[j];
-            if (fileWithLang.pathLang == folderPath) {
+            if (fileWithLang.pathLang === folderPath) {
+                //add the folder name where index is
+                folderName = (function () {
+                    var ee = folderPath.split("/");
+                    var ii = ee.slice(1, -1);
+                    return ii[ii.length - 1] || '';
+                })();
                 //add a layout to the index file
                 if (fileWithLang.indexTemplate) {
                     templateIndexName = fileWithLang.indexTemplate;
@@ -132,38 +90,9 @@ var makeIndex = function (files, metalsmith, done) {
                 if (fileWithLang.fullVideo) {
                     fullVideoPath = fileWithLang.fullVideo;
                 }
-                //check for main sections
-                if (folderUrlTmp.length === 2) {
-                    for (var f in filteredByLang) {
-                        var breakFolderName = filteredByLang[f].split('/');
-                        if (breakFolderName.length === 3) {
-                            section = {title: breakFolderName[1]};
-                            // console.log(fileWithLang)
-                        }
-                    }
-                    section = _.uniq(section);
-                }
-                //check for sectors
-                if (folderUrlTmp.length === 3) {
-                    for (var f in filteredByLang) {
-                        var breakFolderName = filteredByLang[f].split('/');
-                        if (breakFolderName.length === 4) {
-                            sector.push(breakFolderName[2]);
-                        }
-                    }
-                    sector = _.uniq(sector);
-                }
-                //check for subsectors
-                if (folderUrlTmp.length === 4) {
-                    for (var f in filteredByLang) {
-                        var breakFolderName = filteredByLang[f].split('/');
-                        if (breakFolderName.length === 5) {
-                            sector.push(breakFolderName[2]);
-                            subSector.push(breakFolderName[3]);
-                        }
-                    }
-                    sector = _.uniq(sector);
-                    subSector = _.uniq(subSector);
+                //add an intro text to the index file
+                if (fileWithLang.intro) {
+                    intro = fileWithLang.intro;
                 }
                 toIndex += fileWithLang.contents.toString();
                 toToc.push(fileWithLang.title);
@@ -175,11 +104,25 @@ var makeIndex = function (files, metalsmith, done) {
             heroVideo: heroVideoPath,
             heroImage: heroImagePath,
             fullVideo: fullVideoPath,
+            intro: intro,
+            folder: folderName,
             contents: new Buffer(toIndex),
-            section: section,
-            sector: sector,
-            subSector: subSector,
             toc: toToc
+        };
+        levels[i] = {
+            path: folderPath,
+            data: {
+                name: (function () {
+                    var ee = folderPath.split("/");
+                    var ii = ee.slice(1, -1);
+                    return ii[ii.length - 1] || '';
+                })(),
+                thumbnail: thumbnailPath,
+                heroVideo: heroVideoPath,
+                heroImage: heroImagePath,
+                fullVideo: fullVideoPath,
+                intro: intro
+            }
         };
     }
     //delete partial files
@@ -188,8 +131,30 @@ var makeIndex = function (files, metalsmith, done) {
             delete files[file];
         }
     }
+
+    for (var file in files) {
+        var langArray = ['/cn', '/de', '/us'];
+        for (var l = 0; l < langArray.length; l++) {
+            //divide files by lang
+            if (file.match(langArray[l])) {
+                var fileLength = path.dirname(file).split('/').length;
+                var subLevel = [];
+                for (var obj in levels) {
+                    var levelsPath = levels[obj].path;
+                    if (levelsPath.match(langArray[l])) {
+                        var levelLength = levelsPath.split('/').length;
+                        if (fileLength + 1 === levelLength) {
+                            subLevel.push(levels[obj].data);
+                        }
+                    }
+                }
+                files[file].subLevel = subLevel;
+            }
+        }
+    }
     done();
 };
+
 
 Handlebars.registerPartial('header', fs.readFileSync(__dirname + '/layouts/partials/header.html').toString());
 Handlebars.registerPartial('header-nav', fs.readFileSync(__dirname + '/layouts/partials/header-nav.html').toString());
@@ -243,6 +208,15 @@ Handlebars.registerHelper('if_eq', function (a, b, opts) {
         return opts.inverse(this);
 });
 
+// if not equals helpers
+Handlebars.registerHelper('if_ne', function (a, b, opts) {
+    if (a != b) {
+        return opts.fn(this);
+    } else {
+        return opts.inverse(this);
+    }
+});
+
 metalsmith(__dirname)
 // .use(makeIndex)
     .use(layouts({
@@ -263,13 +237,3 @@ metalsmith(__dirname)
     .build(function (err) {
         if (err) console.log(err)
     });
-
-// if not equals helpers
-Handlebars.registerHelper('if_ne', function (a, b, opts) {
-    if (a != b) {
-        return opts.fn(this);
-    } else {
-        return opts.inverse(this);
-    }
-});
-
