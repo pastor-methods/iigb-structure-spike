a8d3ef2cbd0d6866451ea0dc15a77fee50f5086a
pastorjuradotraverso <pastor.methods@gmail.com>
2016-07-19 15:13:43 +0100

    Style ie8

---
+++ build/assets/js/equalHeight.js
@@ -0,0 +1,40 @@
+// Set elements to the same height (uses height of tallest element)
+// taken from https://codepen.io/micahgodbolt/pen/FgqLc
+equalheight = function (container) {
+
+    var currentTallest = 0,
+        currentRowStart = 0,
+        rowDivs = new Array(),
+        $el,
+        topPosition = 0;
+    $(container).each(function () {
+
+        $el = $(this);
+        $($el).height('auto');
+        topPosition = $el.position().top;
+
+        if (currentRowStart != topPosition) {
+            for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
+                rowDivs[currentDiv].height(currentTallest);
+            }
+            rowDivs.length = 0; // empty the array
+            currentRowStart = topPosition;
+            currentTallest = $el.height();
+            rowDivs.push($el);
+        } else {
+            rowDivs.push($el);
+            currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+        }
+        for (currentDiv = 0; currentDiv < rowDivs.length; currentDiv++) {
+            rowDivs[currentDiv].height(currentTallest);
+        }
+    });
+};
+
+$(window).load(function () {
+    equalheight('.js-equal-height');
+});
+
+$(window).resize(function () {
+    equalheight('.js-equal-height');
+});
